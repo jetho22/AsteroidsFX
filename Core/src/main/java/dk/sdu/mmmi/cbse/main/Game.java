707682a1ade2fa5ef5d88cb23d0ff3f6d099ebd3
Jeppe <jetho22@student.sdu.dk>
2024-05-11 21:34:52 +0200

    Spring implemented

---
+++ Core/src/main/java/dk/sdu/mmmi/cbse/main/Game.java
@@ -0,0 +1,200 @@
+package dk.sdu.mmmi.cbse.main;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.GameKeys;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.enemy.Enemy;
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
+
+import java.util.Collection;
+
+import java.util.Map;
+import java.util.ServiceLoader;
+import java.util.concurrent.ConcurrentHashMap;
+import static java.util.stream.Collectors.toList;
+
+import dk.sdu.mmmi.cbse.playersystem.Player;
+import javafx.animation.AnimationTimer;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Polygon;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+public class Game {
+
+    private final GameData gameData = new GameData();
+    private final World world = new World();
+    private final Map<Entity, Polygon> polygons = new ConcurrentHashMap<>();
+    private final Pane gameWindow = new Pane();
+    private long startTime = System.nanoTime();
+    private final Text playerLivesText = new Text();
+    private final Text enemyLivesText = new Text();
+
+    private final List<IGamePluginService> gamePluginServices;
+    private final List<IEntityProcessingService> entityProcessingServiceList;
+    private final List<IPostEntityProcessingService> postEntityProcessingServices;
+
+    Game(List<IGamePluginService> gamePluginServices, List<IEntityProcessingService> entityProcessingServiceList, List<IPostEntityProcessingService> postEntityProcessingServices) {
+        this.gamePluginServices = gamePluginServices;
+        this.entityProcessingServiceList = entityProcessingServiceList;
+        this.postEntityProcessingServices = postEntityProcessingServices;
+    }
+
+    public void start(Stage window) throws Exception {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(ModuleConfig.class);
+
+        for (String beanName : ctx.getBeanDefinitionNames()) {
+            System.out.println(beanName);
+        }
+
+        playerLivesText.setText("Player Lives: " + getPlayerLives());
+        playerLivesText.setX(10);
+        playerLivesText.setY(20);
+        enemyLivesText.setText("Enemy Lives: " + getEnemyLives());
+        enemyLivesText.setX(10);
+        enemyLivesText.setY(40);
+        gameWindow.setPrefSize(gameData.getDisplayWidth(), gameData.getDisplayHeight());
+        gameWindow.getChildren().add(playerLivesText);
+        gameWindow.getChildren().add(enemyLivesText);
+        Scene scene = new Scene(gameWindow);
+
+        scene.setOnKeyPressed(event -> {
+            if (event.getCode().equals(KeyCode.SPACE)) {
+                gameData.getKeys().setKey(GameKeys.SPACE, true);
+            }
+            if (event.getCode().equals(KeyCode.LEFT)) {
+                gameData.getKeys().setKey(GameKeys.LEFT, true);
+            }
+            if (event.getCode().equals(KeyCode.RIGHT)) {
+                gameData.getKeys().setKey(GameKeys.RIGHT, true);
+            }
+            if (event.getCode().equals(KeyCode.UP)) {
+                gameData.getKeys().setKey(GameKeys.UP, true);
+            }
+        });
+        scene.setOnKeyReleased(event -> {
+            if (event.getCode().equals(KeyCode.SPACE)) {
+                gameData.getKeys().setKey(GameKeys.SPACE, false);
+            }
+            if (event.getCode().equals(KeyCode.LEFT)) {
+                gameData.getKeys().setKey(GameKeys.LEFT, false);
+            }
+            if (event.getCode().equals(KeyCode.RIGHT)) {
+                gameData.getKeys().setKey(GameKeys.RIGHT, false);
+            }
+            if (event.getCode().equals(KeyCode.UP)) {
+                gameData.getKeys().setKey(GameKeys.UP, false);
+            }
+
+        });
+
+        for (IGamePluginService iGamePlugin : getGamePluginServices()) {
+            iGamePlugin.start(gameData, world);
+        }
+        for (Entity entity : world.getEntities()) {
+            Polygon polygon = new Polygon(entity.getPolygonCoordinates());
+            polygons.put(entity, polygon);
+            gameWindow.getChildren().add(polygon);
+        }
+
+        render();
+
+        window.setScene(scene);
+        window.setTitle("ASTEROIDS");
+        window.show();
+
+    }
+
+    private int getPlayerLives() {
+        return world.getEntities(Player.class).stream()
+                .findFirst()
+                .map(Entity::getLives)
+                .orElse(0);
+    }
+
+    private int getEnemyLives() {
+        return world.getEntities(Enemy.class).stream()
+                .findFirst()
+                .map(Entity::getLives)
+                .orElse(0);
+    }
+
+    public void render() {
+        new AnimationTimer() {
+            private long lastTime = System.nanoTime();
+
+            @Override
+            public void handle(long now) {
+                // Delta time for physics in Entities
+                float delta = (float) ((now - lastTime) / 1e9);
+                lastTime = now;
+                gameData.setDelta(delta);
+
+                // Animation timer
+                double elapsedTime = (now - startTime) / 1_000_000_000.0;
+                gameData.setElapsedTime(elapsedTime);
+                //System.out.println(elapsedTime);
+
+                update(); // Updates the Entities for the world
+                draw(); // Draws the Polygons
+                gameData.getKeys().update(); // Updates the keys used by the player
+            }
+
+        }.start();
+    }
+
+    private void update() {
+
+        // Update
+        for (IEntityProcessingService entityProcessorService : getEntityProcessingServices()) {
+            entityProcessorService.process(gameData, world);
+        }
+        for (IPostEntityProcessingService postEntityProcessorService : getPostEntityProcessingServices()) {
+            postEntityProcessorService.process(gameData, world);
+        }
+        playerLivesText.setText("Player Lives: " + getPlayerLives());
+        enemyLivesText.setText("Enemy Lives: " + getEnemyLives());
+    }
+
+    private void draw() {
+        for (Entity polygonEntity : polygons.keySet()) {
+            if(!world.getEntities().contains(polygonEntity)){
+                Polygon removedPolygon = polygons.get(polygonEntity);
+                polygons.remove(polygonEntity);
+                gameWindow.getChildren().remove(removedPolygon);
+            }
+        }
+        for (Entity entity : world.getEntities()) {
+            Polygon polygon = polygons.get(entity);
+            if (polygon == null) {
+                polygon = new Polygon(entity.getPolygonCoordinates());
+                polygons.put(entity, polygon);
+
+                gameWindow.getChildren().add(polygon);
+            }
+            polygon.setTranslateX(entity.getX());
+            polygon.setTranslateY(entity.getY());
+            polygon.setRotate(entity.getRotation());
+        }
+    }
+
+    public List<IGamePluginService> getGamePluginServices() {
+        return gamePluginServices;
+    }
+
+    public List<IEntityProcessingService> getEntityProcessingServices() {
+        return entityProcessingServiceList;
+    }
+
+    public List<IPostEntityProcessingService> getPostEntityProcessingServices() {
+        return postEntityProcessingServices;
+    }
+}
--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -4,7 +4,13 @@ module Core {
     requires javafx.graphics;
     requires Player;
     requires CommonEnemy;
-    opens dk.sdu.mmmi.cbse.main to javafx.graphics;
+    requires spring.context;
+    requires spring.beans;
+    requires spring.core;
+
+    exports dk.sdu.mmmi.cbse.main;
+
+    opens dk.sdu.mmmi.cbse.main to javafx.graphics, spring.core;
     uses dk.sdu.mmmi.cbse.common.services.IGamePluginService;
     uses dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
     uses dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
